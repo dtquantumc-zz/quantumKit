3ae5e295ec6e13573eb849143a6925bfb75149fe
Ari Blondal <ari.blondal@gmail.com>
2020-07-14 12:17:58 -0700

    [Fixed Pickle Checking] Now uses area collided to get owner and pickle count. Does not rely on scene tree anymore :D

--- Games/quantum-navigator/QuantumNavigator/Computer/Computer.gd
+++ Games/quantum-navigator/QuantumNavigator/Computer/Computer.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-func create_computer_effect():
+func create_computer_effect(toTeleport):
 	var ComputerEffect = load("res://Effects/ComputerEffect.tscn")
 	var computerEffect = ComputerEffect.instance()
 	var world = get_tree().current_scene
 	world.add_child(computerEffect)
 	computerEffect.global_position = global_position
+	computerEffect.connectTeleport( toTeleport )
 
 func _on_Hurtbox_area_entered(_area):
-	if ($'/root/World/YSort/Otter'.stats.pickles >= 2):
-		$'/root/World/YSort/Otter'.stats.pickles -= 2
-		create_computer_effect()
+	print(_area.owner.stats.pickles)
+	if (_area.owner.stats.pickles >= 2):
+		_area.owner.stats.pickles -= 2
+		create_computer_effect(_area.owner)
 		queue_free()
--- Games/quantum-navigator/QuantumNavigator/Effects/ComputerEffect.gd
+++ Games/quantum-navigator/QuantumNavigator/Effects/ComputerEffect.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 onready var animatedSprite = $AnimatedSprite
 
+var toTeleport = null
+
 signal computer_effect_done(position)
 signal computer_effect_start
 
 func _ready():
 	animatedSprite.frame = 0
 	animatedSprite.play("Animate")
+
+func connectTeleport(toTeleport):
 	self.connect("computer_effect_done",
-		$"/root/World/YSort/Otter",
+		toTeleport,
 		"_on_Computer_effect_process_done")
-	$"/root/World/YSort/Otter".call("_on_Computer_effect_process_start")
+	toTeleport.call("_on_Computer_effect_process_start")
 
 func _on_AnimatedSprite_animation_finished():
 	emit_signal("computer_effect_done", position)
