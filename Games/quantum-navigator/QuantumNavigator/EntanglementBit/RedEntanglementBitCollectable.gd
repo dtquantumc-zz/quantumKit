da4268c01b7fd2a22cfbbe7171288d2d3bd3812c
Tanraj <tsaran70@gmail.com>
2020-07-16 15:16:24 -0700

    [#16] Adding Info Boxes
    What: Add info boxes for
    - The teleporter
    - The encoder
    - The decoder
    - The fire hazard
    - Bell pairs
    
    Source for the Dialog System
    used for the info boxes:
    https://godotengine.org/asset-library/asset/628
    
    PS2:
    - Removed decoder info box code.
    This will be added when it's description
    is finalized.
    - Finished info boxes for the
    remaining components.
    - Removed the SPACEBAR key from
    shooting qubits so the Otter won't shoot
    when going through info box nodes
    
    - TODO: Need to handle case where
    multiple info boxes are available at
    the same time
    
    - TODO: Since the SPACEBAR key might
    be most intuitive for shooting, re-introduce
    it for shooting and handle the case when the
    info box is open

--- Games/quantum-navigator/QuantumNavigator/EntanglementBit/RedEntanglementBitCollectable.gd
+++ Games/quantum-navigator/QuantumNavigator/EntanglementBit/RedEntanglementBitCollectable.gd
@@ -2,12 +2,31 @@ extends KinematicBody2D
 
 const PickupItemEffect = preload("res://Effects/PickUpItemEffect.tscn")
 
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var dialogPlayer = $Dialog_Player
+
+var bit_collected = false
+
+func _physics_process(_delta):
+	if !bit_collected && playerDetectionZone.can_see_player():
+		OtterStats.set_can_see_red_bell_pair({"name": get_name(), "value": true})
+		if Input.is_action_just_pressed("info"):
+			dialogPlayer.play_dialog("BellPairsInfoBox")
+	else:
+		OtterStats.set_can_see_red_bell_pair({"name": get_name(), "value": false})
+		dialogPlayer.stop_dialog()
+
 func create_pickupitem_effect():
 	var pickupItemEffect = PickupItemEffect.instance()
 	get_parent().add_child(pickupItemEffect)
-    pickupItemEffect.global_position = global_position
+	pickupItemEffect.global_position = global_position
 
 func _on_Hurtbox_area_entered(_area):
-    OtterStats.red_bits += 1
-    create_pickupitem_effect()
+	OtterStats.red_bits += 1
+	create_pickupitem_effect()
+
+	OtterStats.set_can_see_red_bell_pair({"name": get_name(), "value": false})
+	dialogPlayer.stop_dialog()
+	bit_collected = true
+
 	queue_free()
---
+++ Games/quantum-navigator/QuantumNavigator/UI/RedBellPairHelpUI.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var red_bell_pair_visible = {} setget set_red_bell_pair_visible
+
+onready var icon = $Icon
+onready var label = $Label
+
+func set_red_bell_pair_visible(dict):
+	red_bell_pair_visible[dict.name] = dict.value
+	if red_bell_pair_visible.values().has(true):
+		icon.rect_size.x = 28
+		icon.rect_size.y = 14
+		label.set_percent_visible(1)
+	else:
+		icon.rect_size.x = 0
+		icon.rect_size.y = 0
+		label.set_percent_visible(0)
+
+func _ready():
+	# warning-ignore:return_value_discarded
+	OtterStats.connect("red_bell_pair_visible", self, "set_red_bell_pair_visible")
+
+
