53bd6bed902c2473a86fb6105866d65732aaf2c5
Ari Blondal <ari.blondal@gmail.com>
2020-07-17 13:22:40 -0700

    [Decoder Info shows even after use]

--- Games/quantum-navigator/QuantumNavigator/PlayerDetectionZone.gd
+++ Games/quantum-navigator/QuantumNavigator/PlayerDetectionZone.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
 var player = null
+var players = []
 
 func can_see_player():
-    return player != null
+	return player != null
 
 func _on_PlayerDetectionZone_body_entered(body):
-    player = body
+	player = body
+	players.append(body)
 
 func _on_PlayerDetectionZone_body_exited(_body):
-    player = null
+	players.erase(_body)
+	if players.size() == 0:
+		player = null
+	else:
+		player = players[0]
