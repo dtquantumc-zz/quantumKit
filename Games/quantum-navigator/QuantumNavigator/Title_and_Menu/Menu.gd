35f88fc0dd184023f8063391a0605d85873d1cbe
Tanraj <tsaran70@gmail.com>
2020-07-17 15:35:56 -0700

    [#17] Add Game Music + Sound Effects
    What:
    - Added Otter die and
    Boulder Explosion effects (and
    connected the Right Boulder to
    explode once the decoder is used)
    
    PS2:
    - Fixed flash (Otter death) effect so
    it is unique for each Otter and doesn't
    effect the encoded Otters
    - Added bit and pickle collection, walking, shooting,
    error when trying to open the teleporter and
    menu button sound effects
    - Made the Otter shadow visible
    - Slightly tweaked the positioning of the
    
    PS3:
    - Added background music
    - Fixed bug where Teleporter error sound
    wasn't playing after the teleporter was used
    
    PS4:
    - Added mute option for background music
    - Made Fire Trap info box visible

--- Games/quantum-navigator/QuantumNavigator/Title_and_Menu/Menu.gd
+++ Games/quantum-navigator/QuantumNavigator/Title_and_Menu/Menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var MenuItems = ["Start_Item", "Exit_Item"]
+var MenuItems = ["Start_Item", "Exit_Item", "Mute_Item"]
 var Selected = 0
 
 var MenuConfirmSound = preload("res://Title_and_Menu/MenuConfirmSound.tscn")
@@ -11,7 +11,6 @@ export(PackedScene) var startScene = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	BackgroundMusic.start_music()
-	pass
 
 func set_selection(val):
 	if Selected == val: return
@@ -46,5 +45,10 @@ func _process(delta):
 		if Selected == 0 and startScene != null:
 			get_tree().change_scene_to(startScene)
 			OtterStats.reset()
+		elif Selected == 2 and startScene != null:
+			if BackgroundMusic.is_playing():
+				BackgroundMusic.stop_music()
+			else:
+				BackgroundMusic.start_music()
 		else:
 			get_tree().quit()
--- Games/quantum-navigator/QuantumNavigator/World/BackgroundMusic.gd
+++ Games/quantum-navigator/QuantumNavigator/World/BackgroundMusic.gd
@@ -4,3 +4,9 @@ onready var audioStreamPlayer = $AudioStreamPlayer
 
 func start_music():
 	audioStreamPlayer.play()
+
+func stop_music():
+	audioStreamPlayer.stop()
+
+func is_playing():
+	return audioStreamPlayer.is_playing()
