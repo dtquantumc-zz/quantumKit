265567c4afacbab16de3cf1724b142a832a1e64f
Z0rb14n <babybluewoof@gmail.com>
2021-08-02 23:05:51 -0700

    Made vibrations slightly more organic and added preliminary particles

--- Games/quantum-navigator/QuantumNavigator/VibratingTile/VibratingTile.gd
+++ Games/quantum-navigator/QuantumNavigator/VibratingTile/VibratingTile.gd
@@ -4,11 +4,24 @@ extends Node
 
 # Note: $<Node-name> is shorthand for get_node(<Node-name>)
 onready var playerDetectionZone = $PlayerDetectionZone
-onready var animationPlayer = $AnimationPlayer
+onready var vibrationPlayer = $VibrationPlayer
 onready var dialogPlayer = $Dialog_Player
 onready var vibratingTileHitbox = $Hitbox
 onready var timer = $Timer
+onready var particles = $Particles2D
 
+# List of slow vibrations
+export(Array, String) var SlowAnimations : Array = ["VibrateSlow","VibrateSlow2"]
+# List of Fast Vibrations
+export(Array, String) var FastAnimations : Array = ["VibrateFast"]
+# Particles when slow vibrating
+export(float) var ParticleSlowVibrating : float = 8
+# Particle speed when slow vibrating
+export(float) var ParticleSlowSpeed : float = 12
+# Particles when fast vibrating
+export(float) var ParticleFastVibrating : float = 16
+# Particle speed when fast vibrating
+export(float) var ParticleFastSpeed : float = 25
 # Time when slowly vibrating
 export(float) var TimeSlowVibrate : float= 3
 # Time of vibration (to kill)
@@ -24,7 +37,13 @@ var otterInside = null setget _set_otter
 # 0 = not vibrating, 1 = vibrating slightly, 2 = kill, 3 = cooldown
 var state : int = 0
 
+var rng = RandomNumberGenerator.new()
 # TODO add particle/stone effect around vibrating tile
+
+# Called when the node enters the scene tree for the first time
+# Initializes the RNG
+func _ready():
+	rng.randomize()
 
 # Internally sets the otter, and if the otter is on this object while vibrating,
 # make it take damage
@@ -35,6 +54,19 @@ func _set_otter(otter):
 			print("Vibrating Tile _set_otter Otter name: " + otterInside.get_name())
 		otterInside.take_damage()
 
+# Utility to play an animation in reverse randomly
+func _play_random_reverse(var animName : String) -> void:
+	var reverse = _random_reverse()
+	if (reverse):
+		vibrationPlayer.play_backwards(animName)
+	else:
+		vibrationPlayer.play(animName)
+
+# Utility to return a random boolean
+func _random_reverse() -> bool:
+	var result = rng.randi_range(0,1)
+	return result == 1
+
 # Begin the timer for vibrating, if it can
 func start_vibrating():
 	if ForceNeverVibrate:
@@ -42,7 +74,11 @@ func start_vibrating():
 	if state == 0:
 		state = 1
 		timer.wait_time = TimeSlowVibrate
-		animationPlayer.play("VibrateSlow")
+		var index = rng.randi_range(0,SlowAnimations.size()-1)
+		_play_random_reverse(SlowAnimations[index])
+		particles.emitting = true
+		particles.amount = ParticleSlowVibrating
+		particles.process_material.initial_velocity = ParticleSlowSpeed
 		if (Debug):
 			print("Vibrating soon...")
 		timer.start()
@@ -54,13 +90,17 @@ func _on_timer_timeout():
 		timer.wait_time = TimeFullVibrate
 		if (Debug):
 			print("Vibrating")
-		animationPlayer.play("VibrateFast")
+		var index = rng.randi_range(0,FastAnimations.size()-1)
+		_play_random_reverse(FastAnimations[index])
+		particles.amount = ParticleFastVibrating
+		particles.process_material.initial_velocity = ParticleFastSpeed
 		timer.start()
 		if otterInside != null:
 			otterInside.take_damage()	
 		state = 2
 	elif state == 2:
-		animationPlayer.play("IdleAnim")
+		vibrationPlayer.play("IdleAnim")
+		particles.emitting = false
 		timer.wait_time = MinTimeBeforeVibrate
 		timer.start()
 		if (Debug):
