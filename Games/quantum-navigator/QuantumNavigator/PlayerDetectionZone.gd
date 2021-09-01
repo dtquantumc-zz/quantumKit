fbc1f43d95bfaebae1af1972190848f68d4023b7
Z0rb14n <babybluewoof@gmail.com>
2021-08-31 22:19:14 -0700

    Fix button double press and player detection zone player detection

--- Games/quantum-navigator/QuantumNavigator/PlayerDetectionZone.gd
+++ Games/quantum-navigator/QuantumNavigator/PlayerDetectionZone.gd
@@ -22,16 +22,18 @@ func can_see_player() -> bool:
 func _on_PlayerDetectionZone_body_entered(body):
 	if (Debug):
 		print("[PlayerDetectionZone _on_PDZ_body_entered]: " + str(body))
-	player = body
-	players.append(body)
+	if ("Otter" in body.name):
+		player = body
+		players.append(body)
 
 # Called upon object exiting detection zone
 # Remove exiting body from the array of objects currently in the zone
-func _on_PlayerDetectionZone_body_exited(_body):
+func _on_PlayerDetectionZone_body_exited(body):
 	if (Debug):
-		print("[PlayerDetectionZone _on_PDZ_body_exited]: " + str(_body))
-	players.erase(_body)
-	if players.size() == 0:
-		player = null
-	else:
-		player = players[0]
+		print("[PlayerDetectionZone _on_PDZ_body_exited]: " + str(body))
+	if ("Otter" in body.name):
+		players.erase(body)
+		if players.size() == 0:
+			player = null
+		else:
+			player = players[0]
